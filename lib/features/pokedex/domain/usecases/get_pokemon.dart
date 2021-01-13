9a05e46d3702a61d6f81b742a97e711a717838fd
Ricardo Sohn <ricardosohn@gmail.com>
2021-01-13 08:43:42 -0300

    [GREEN] Created GetPokemon usecase to pass tests

---
+++ lib/features/pokedex/domain/usecases/get_pokemon.dart
@@ -0,0 +1,16 @@
+import 'package:FlutterDex/features/pokedex/domain/entities/Pokemon.dart';
+import 'package:FlutterDex/features/pokedex/domain/repositories/pokemon_repository.dart';
+import 'package:FlutterDex/core/error/failures.dart';
+import 'package:dartz/dartz.dart';
+import 'package:meta/meta.dart';
+
+class GetPokemon {
+  final PokemonRepository repository;
+
+  GetPokemon(this.repository);
+
+  Future<Either<Failure, Pokemon>> execute(
+      {@required String pokemonName}) async {
+    return await repository.getPokemon(pokemonName);
+  }
+}
